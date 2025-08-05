7ebbe04214c1919a3e2552ebd2e3f2ad1185ebe1
Mena Erian <mena3koko2karas1@gmail.com>
2025-08-05 11:49:03 +0300

    Generic

---
+++ Demo/MyCollection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class MyCollection<T>
+    {
+        private T[] arr;
+        private int currentIndex = -1;
+        public MyCollection()
+        {
+            arr = new T[5];
+            currentIndex = -1;
+        }
+
+        public void Add(T value)
+        {
+            //T t = new T();
+            if (currentIndex == arr.Length - 1) Extend();
+            currentIndex++;
+            arr[currentIndex] = value;
+        }
+
+        private void Extend()
+        {
+            T[] newArr = new T[arr.Length * 2];
+            for (int i = 0; i < arr.Length; i++)
+                newArr[i] = this.arr[i];
+            this.arr = newArr;
+        }
+
+        public T GetByIndex(int index)
+        {
+            if (index < 0 || index > currentIndex) throw new IndexOutOfRangeException();
+            return arr[index];
+        }
+        public void Print()
+        {
+            for (int i = 0; i < arr.Length; i++)
+                Console.WriteLine(arr[i]);
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 
+            //string str = "1a3";
+            //int x;
+            //int.TryParse(str, out x); 
+
+            //Employee e1 = new Employee() { Id = 10, Name = "Mena" }; 
+            #endregion
+
+            #region Generic
+            /// int[] arr = { 1, 2, 3, 4, 5 };
+            /// 
+            /// MyCollection<int> myCollection = new MyCollection<int>();
+            /// 
+            /// MyCollection<string> myCollection1;
+            /// 
+            /// myCollection.Add(1);
+            /// myCollection.Add(2);
+            /// myCollection.Add(3);
+            /// myCollection.Add(4);
+            /// myCollection.Add(5);
+            /// 
+            /// //int n = (int)myCollection.GetByIndex(2);
+            /// myCollection.Print();
+            #endregion
         }
     }
 }
